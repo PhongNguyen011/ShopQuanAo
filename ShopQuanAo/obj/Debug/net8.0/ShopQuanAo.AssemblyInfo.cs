a4a2d727458010b1e9cba92cd3509a0fad6b8d21
hoangkimngoc <134307965+hoangkimngoc@users.noreply.github.com>
2025-10-24 12:11:45 +0700

    fix

--- ShopQuanAo/obj/Debug/net8.0/ShopQuanAo.AssemblyInfo.cs
+++ ShopQuanAo/obj/Debug/net8.0/ShopQuanAo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopQuanAo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f4be8d6df78f4949daa835b2c0e2df7f75d775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7737e0a61e3c3dc06764520beab663cf20576fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopQuanAo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopQuanAo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
