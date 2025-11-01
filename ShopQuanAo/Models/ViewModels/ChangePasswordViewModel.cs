c841a9a37c074dfce0ee9b6211b7b4caaab7eb84
ngtutai <cuem1235879@gmail.com>
2025-11-01 17:59:48 +0700

    Fix Layout + Add ChangePassword
    Sửa layout 1 tí cho các trang
    Sửa layout cho form Checkout
    Thêm trang đổi mk (user)

---
+++ ShopQuanAo/Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopQuanAo.Models.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu hiện tại")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu hiện tại")]
+        public string OldPassword { get; set; } = string.Empty;
+
+        [MinLength(6, ErrorMessage = "Mật khẩu mới tối thiểu 6 ký tự")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu mới")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu nhập lại không khớp")]
+        [Display(Name = "Nhập lại mật khẩu mới")]
+        [Required(ErrorMessage = "Vui lòng nhập lại mật khẩu mới")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
